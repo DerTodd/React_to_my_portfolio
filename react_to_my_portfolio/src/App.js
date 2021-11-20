00b1ab84bed551c6e9685753170f7ea7f6a2892e
DerTodd <88950531+DerTodd@users.noreply.github.com>
2021-11-19 22:04:03 -0600

    initial nav bar

--- react_to_my_portfolio/src/App.js
+++ react_to_my_portfolio/src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form/index';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Card from './components/Card';
 
 function App() {
   return (
-    <div className="App">
+    <><div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -11,14 +16,20 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
-    </div>
+    </div><div>
+        <Header />
+        <Navbar />
+        <Card />
+        <Card />
+        <Card />
+        <Form />
+      </div></>
   );
 }
 
