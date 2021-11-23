5ab5fb5f4a2752cabe9dce07f7239104c3d3ca5d
DerTodd <88950531+DerTodd@users.noreply.github.com>
2021-11-23 17:22:34 -0600

    final updates with icons and images

--- react_to_my_portfolio/src/components/Section.js
+++ react_to_my_portfolio/src/components/Section.js
@@ -9,6 +9,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import GitHub from '../images/GitHubMark32px.png'
+import Rome from "../images/rome400.png"
+import Moose from "../images/moosesnow800.png"
 
 
 // By importing the Section.css file, it is added to the DOM whenever this component loads
@@ -33,7 +35,7 @@ function Section() {
     <section style={styles.sectionStyles} className="container-fluid">
       
         <Container id="samples" style={styles.sectionStyles} className="mb-4 bg-light rounded-3">
-            
+                <h6>Food Buzz</h6>
                     <a href="https://dertodd.github.io/Project_one_The_Survivors/index.html" target="_blank" rel="noopener noreferrer"><img src={FoodBuzz} alt='FoodBuzz page' className="img-fluid rounded hover-shadow" /></a>
                     <div className='mask' style={{ 
                         backgroundColor: 'rgba(0, 0, 0, 0.6)', 
@@ -52,7 +54,7 @@ function Section() {
                     <Col lg={true} style={{borderWidth: 8, borderBlockColor: '#BF5700', borderBlockStyle: 'solid', borderLeft: 8}}>
                 <figure className="col">
                     <div>
-                    
+                    <h6>Whether Weather</h6>
                         <a href="https://dertodd.github.io/whether_weather_TT/" target="_blank" rel="noopener noreferrer"><img src={Weather} alt="Weather App" className="img-fluid rounded hover-shadow" /></a>
                         <div className='mask' style={{ 
                         backgroundColor: 'rgba(0, 0, 0, 0.6)', 
@@ -68,6 +70,7 @@ function Section() {
                 <Col lg={true} style={{borderWidth: 8, borderBlockColor: '#BF5700', borderBlockStyle: 'solid'}}>
                 <figure className="col">
                     <div>
+                    <h6>Planner</h6>
                         <a href="https://dertodd.github.io/Daily_Planner_TT/" target="_blank" rel="noopener noreferrer"><img className="img-fluid rounded hover-shadow" src={Planner} alt="Day Planner" /></a>
                         <div className='mask' style={{ 
                         backgroundColor: 'rgba(0, 0, 0, 0.6)', 
@@ -85,6 +88,7 @@ function Section() {
                 <Col lg={true} style={{borderWidth: 8, borderBlockColor: '#BF5700', borderBlockStyle: 'solid'}}>
                 <figure className="col">
                     <div>
+                    <h6>Sweepers</h6>
                     <a href="https://sweeper-chore-chart.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img className="img-fluid rounded hover-shadow" src={Sweeper} alt="Sweepers Site" /></a>
                         <div className='mask' style={{ 
                         backgroundColor: 'rgba(0, 0, 0, 0.6)', 
@@ -100,6 +104,7 @@ function Section() {
                 <Col lg={true} style={{borderWidth: 8, borderBlockColor: '#BF5700', borderBlockStyle: 'solid'}}>
                 <figure className="col">
                     <div>
+                    <h6>Bottled Up</h6>
                     <a href="https://bottleduptt.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img className="img-fluid rounded hover-shadow" src={Bottled} alt="bottled up site" /></a>
                         <div className='mask' style={{ 
                         backgroundColor: 'rgba(0, 0, 0, 0.6)', 
@@ -117,12 +122,15 @@ function Section() {
             <Row>
                 <Col lg={true} style={{borderWidth: 8, borderBlockColor: '#BF5700', borderBlockStyle: 'solid'}}>
                 <figure className="col">
-                    <div>
-                    <a href="https://dertodd.github.io/Quiz_Time_TT/" target="_blank" rel="noopener noreferrer"><img className="img-fluid rounded hover-shadow" src={Bottled} alt="bottled up site" /></a>
+                    <div >
+                    <h6>Quiz Time</h6>
+                    
+                    <a href="https://dertodd.github.io/Quiz_Time_TT/" target="_blank" rel="noopener noreferrer"><img className="img-fluid rounded hover-shadow" src={Moose} alt="bottled up site" /></a>
                         <div className='mask' style={{ 
                         backgroundColor: 'rgba(0, 0, 0, 0.6)', 
                         padding: '0,0,0,0'
                         }}>
+                          
                             <div className='d-flex justify-content-center align-items-center h-100'>
                             <p className='text-white mb-0'><a href="https://github.com/DerTodd/Quiz_Time_TT" target="_blank" rel="noopener noreferrer"><img src={GitHub} alt='GitHub'/></a></p>
                             </div>
