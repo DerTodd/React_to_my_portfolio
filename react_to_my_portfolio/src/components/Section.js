9631cd7dd522ed5f9c01c15dc15e3c74c9356af5
DerTodd <88950531+DerTodd@users.noreply.github.com>
2021-11-22 20:08:49 -0600

    working on bootstrap.

--- react_to_my_portfolio/src/components/Section.js
+++ react_to_my_portfolio/src/components/Section.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React , { Component }from 'react';
 import '../styles/Section.css';
 import FoodBuzz from '../images/FoodBuzz.jpg'
 import Planner from '../images/Planner.jpg'
 import Sweeper from '../images/Sweeper.jpg'
 import Weather from '../images/Weather.png'
 import Bottled from '../images/Welcome_Screen.jpg'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import GitHub from '../images/GitHubMark32px.png'
+
 
 // By importing the Section.css file, it is added to the DOM whenever this component loads
 
@@ -15,7 +20,8 @@ import Bottled from '../images/Welcome_Screen.jpg'
 
 const styles = {
   sectionStyles: {
-    background: 'orange',
+    background: '#BF5700',
+    padding: '0',
   },
 };
 
@@ -26,60 +32,107 @@ function Section() {
     <div>
     <section style={styles.sectionStyles} className="container-fluid">
       <h2>Work</h2>
-        <div id="samples" className="row">
+        <Container id="samples" style={styles.sectionStyles} className="mb-4 bg-light rounded-3">
             
-                    <div className="col-sm-10 col-md-10 col-lg-10 col-xl-12">
-                        <a href="https://dertodd.github.io/Project_one_The_Survivors/index.html"><img className="eight flip" src={FoodBuzz} alt="FoodBuzz" /></a>
-                        
-                        <div className="float">
-                            <p className="inside"><a href="https://dertodd.github.io/Project_one_The_Survivors/index.html">FoodBuzz</a></p>
-                            <p className="inside">FoodBuzz</p>
-                        </div>
-                    </div>
-                
+                    <a href="https://dertodd.github.io/Project_one_The_Survivors/index.html"><img src={FoodBuzz} className="img-fluid rounded hover-shadow" /></a>
+                    <div className='mask' style={{ 
+                        backgroundColor: 'rgba(0, 0, 0, 0.6)', 
+                        padding: '0,0,0,0'
+                        }}>
+        <div className='d-flex justify-content-center align-items-center h-100'>
+          <p className='text-white mb-0'><a href="https://github.com/DerTodd/Project_one_The_Survivors"><img src={GitHub} /></a></p>
         </div>
+      </div>
+                
+             
+        </Container>
         <div className="row">
-            <div id="line2" className="samplebox">
+            <Container id="samples" style={styles.sectionStyles} className="mb-4 bg-light rounded-3">
+                <Row>
+                    <Col lg={true} style={{borderWidth: 8, borderBlockColor: '#BF5700', borderBlockStyle: 'solid', borderLeft: 8}}>
                 <figure className="col">
                     <div>
                     
-                        <a href="https://dertodd.github.io/whether_weather_TT/"><img className="four flip" src={Weather} alt="image of a bear" /></a>
-                        <div className="floatsm">
-                            <p className="inside"><a href="https://dertodd.github.io/whether_weather_TT/">Whether Weather</a></p>
-                            <p className="inside">Whether Weather</p>
+                        <a href="https://dertodd.github.io/whether_weather_TT/"><img src={Weather} alt="Weather App" className="img-fluid rounded hover-shadow" /></a>
+                        <div className='mask' style={{ 
+                        backgroundColor: 'rgba(0, 0, 0, 0.6)', 
+                        
+                        }}>
+                            <div className='d-flex justify-content-center align-items-center h-100'>
+                            <p className='text-white mb-0'><a href="https://github.com/DerTodd/whether_weather_TT"><img src={GitHub} /></a></p>
+                            </div>
                         </div>
                     </div>
                 </figure>
+                </Col>
+                <Col lg={true} style={{borderWidth: 8, borderBlockColor: '#BF5700', borderBlockStyle: 'solid'}}>
                 <figure className="col">
                     <div>
-                        <a href="https://dertodd.github.io/Daily_Planner_TT/"><img className="four flip" src={Planner} alt="Day Planner" /></a>
-                        <div className="floatsm">
-                            <p className="inside"><a href="https://dertodd.github.io/Daily_Planner_TT/">Planner</a></p>
-                            <p className="inside">Day Planner</p>
+                        <a href="https://dertodd.github.io/Daily_Planner_TT/"><img className="img-fluid rounded hover-shadow" src={Planner} alt="Day Planner" /></a>
+                        <div className='mask' style={{ 
+                        backgroundColor: 'rgba(0, 0, 0, 0.6)', 
+                        padding: '0,0,0,0'
+                        }}>
+                            <div className='d-flex justify-content-center align-items-center h-100'>
+                            <p className='text-white mb-0'><a href="https://github.com/DerTodd/Daily_Planner_TT"><img src={GitHub} /></a></p>
+                            </div>
                         </div>  
                     </div>
                 </figure>
-            </div>
-            <div id="line3" className="samplebox">
+                </Col>
+            </Row>
+            <Row>
+                <Col lg={true} style={{borderWidth: 8, borderBlockColor: '#BF5700', borderBlockStyle: 'solid'}}>
                 <figure className="col">
                     <div>
-                        <img className="flip four" src={Sweeper} alt="sweeper site" />
-                        <div className="floatsm">
-                            <p className="inside"><a href="https://sweeper-chore-chart.herokuapp.com/">Sweepers</a></p>
-                            <p className="inside">Keep It Clean</p>
+                    <a href="https://sweeper-chore-chart.herokuapp.com/"><img className="img-fluid rounded hover-shadow" src={Sweeper} alt="Sweepers Site" /></a>
+                        <div className='mask' style={{ 
+                        backgroundColor: 'rgba(0, 0, 0, 0.6)', 
+                        padding: '0,0,0,0'
+                        }}>
+                            <div className='d-flex justify-content-center align-items-center h-100'>
+                            <p className='text-white mb-0'><a href="https://github.com/DerTodd/Sweepers_Project_Two"><img src={GitHub} /></a></p>
+                            </div>
                         </div>
                     </div>
                 </figure>
+                </Col>
+                <Col lg={true} style={{borderWidth: 8, borderBlockColor: '#BF5700', borderBlockStyle: 'solid'}}>
                 <figure className="col">
                     <div>
-                        <img className="flip four" src={Bottled} alt="bottled up site" />
-                        <div className="floatsm">
-                            <p className="inside"><a href="https://bottleduptt.herokuapp.com/">Bottled Up</a></p>
-                            <p className="inside">Blog It</p>
+                    <a href="https://bottleduptt.herokuapp.com/"><img className="img-fluid rounded hover-shadow" src={Bottled} alt="bottled up site" /></a>
+                        <div className='mask' style={{ 
+                        backgroundColor: 'rgba(0, 0, 0, 0.6)', 
+                        padding: '0,0,0,0'
+                        }}>
+                            <div className='d-flex justify-content-center align-items-center h-100'>
+                            <p className='text-white mb-0'><a href="https://github.com/DerTodd/Bottled_Up_TT"><img src={GitHub} /></a></p>
+                            </div>
                         </div>
+                        
                     </div>    
                 </figure>
-            </div>  
+                </Col>
+            </Row>
+            <Row>
+                <Col lg={true} style={{borderWidth: 8, borderBlockColor: '#BF5700', borderBlockStyle: 'solid'}}>
+                <figure className="col">
+                    <div>
+                    <a href="https://dertodd.github.io/Quiz_Time_TT/"><img className="img-fluid rounded hover-shadow" src={Bottled} alt="bottled up site" /></a>
+                        <div className='mask' style={{ 
+                        backgroundColor: 'rgba(0, 0, 0, 0.6)', 
+                        padding: '0,0,0,0'
+                        }}>
+                            <div className='d-flex justify-content-center align-items-center h-100'>
+                            <p className='text-white mb-0'><a href="https://github.com/DerTodd/Quiz_Time_TT"><img src={GitHub} /></a></p>
+                            </div>
+                        </div>
+                        
+                    </div>    
+                </figure>
+                </Col>
+            </Row>
+            </Container>  
         </div>  
     </section>
     </div>
